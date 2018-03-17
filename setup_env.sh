7c7b4147727a0be8dda29950f6066bda3980c430
Rodolphe CARGOU <rcargou@e1r12p11.42.fr>
2018-03-17 15:16:57 +0100

    Push the old git data

---
+++ setup_env.sh
@@ -0,0 +1,8 @@
+export PATH=~/.brew/bin:$PATH
+export CPATH=~/.brew/include:$CPATH
+export CPATH=~/.brew/include/GLFW:$CPATH
+export CPATH=~/.brew/include/SFML:$CPATH
+export LD_LIBRARY_PATH=./assimp/lib
+export LD_LIBRARY_PATH=~/.brew/lib/:$LD_LIBRARY_PATH
+
+
